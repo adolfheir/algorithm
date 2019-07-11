5f3f3a212390029c0a3d253193962a123efbadf8
wb-cyh578854 <wb-cyh578854@alibaba-inc.com>
2019-07-11 12:39:20 +0800

    feat:add bezier

---
+++ other/bezier.js
@@ -0,0 +1,26 @@
+/**
+ * @desc 三阶贝塞尔
+ * @param {number} t 当前百分比
+ * @param {Array} p1 起点坐标
+ * @param {Array} p2 终点坐标
+ * @param {Array} cp1 控制点1
+ * @param {Array} cp2 控制点2
+ */
+export function threeBezier(t, p1, cp1, cp2, p2) {
+    const [x1, y1] = p1;
+    const [x2, y2] = p2;
+    const [cx1, cy1] = cp1;
+    const [cx2, cy2] = cp2;
+    let x =
+      x1 * (1 - t) * (1 - t) * (1 - t) +
+      3 * cx1 * t * (1 - t) * (1 - t) +
+      3 * cx2 * t * t * (1 - t) +
+      x2 * t * t * t;
+    let y =
+      y1 * (1 - t) * (1 - t) * (1 - t) +
+      3 * cy1 * t * (1 - t) * (1 - t) +
+      3 * cy2 * t * t * (1 - t) +
+      y2 * t * t * t;
+    return [x, y];
+  }
+  
